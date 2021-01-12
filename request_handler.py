8bea1a884c733f0c0d95bc328c1a1eded9e6caa3
Danny McCracken <dannymccracken13@yahoo.com>
2021-01-12 13:25:06 -0600

    created post method functionality for all modules

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
 from locations import get_all_locations, get_single_location, create_location
-from employees import get_all_employees, get_single_employee
-from customers import get_all_customers, get_single_customer
+from employees import get_all_employees, get_single_employee, create_employee
+from customers import get_all_customers, get_single_customer, create_customer
 import json
 
 # Here's a class. It inherits from another class.
@@ -101,20 +101,25 @@ class HandleRequests(BaseHTTPRequestHandler):
         (resource, id) = self.parse_url(self.path)
 
         # Initialize new animal
-        new_animal = None
+        new_entry = None
 
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
         if resource == "animals":
-            new_animal = create_animal(post_body)
+            new_entry = create_animal(post_body)
 
         if resource == "locations":
-            new_location = create_location(post_body)
+            new_entry = create_location(post_body)
 
-        # Encode the new animal and send in response
-        self.wfile.write(f"{new_animal}".encode())
+        if resource == "employees":
+            new_entry = create_employee(post_body)
 
+        if resource == "customers":
+            new_entry = create_customer(post_body)
+
+        # Encode the new entry and send in response
+        self.wfile.write(f"{new_entry}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
