6d2c28fa1466612abe1b32a45a5085930abe86cd
Danny McCracken <dannymccracken13@yahoo.com>
2021-01-15 15:52:59 -0600

    completed all of chapter 8 and chapter 9, creating a database and all module queries

--- models/customer.py
+++ models/customer.py
@@ -1,10 +1,11 @@
-class Customer:
+class Customer():
 
-    # Class initializer. It has 4 custom parameters, with the
+    # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, name, email, password):
+    def __init__(self, id, name, address, email, password):
         self.id = id
         self.name = name
+        self.address = address
         self.email = email
         self.password = password
--- models/location.py
+++ models/location.py
@@ -1,5 +1,9 @@
-class Location:
+class Location():
 
-    def __init__(self, id, address):
+    # Class initializer. It has 5 custom parameters, with the
+    # special `self` parameter that every method on a class
+    # needs as the first parameter.
+    def __init__(self, id, name, address):
         self.id = id
+        self.name = name
         self.address = address
